98092f2d80fcea99e5545c4b0a6a62f9065dceaa
Bortiz1993 <brijido.zuniga@bryanuniversity.edu>
2022-08-29 04:34:09 -0500

    Updated code

--- mainGame/index.js
+++ mainGame/index.js
@@ -18,9 +18,61 @@ playerImageDown.src = "../img/playerDown.png"
 //I want the player image to load here
 image.onload = () => {
     //x coordinate and Y coordinates
-c.drawImage(image, -300, -300);
+c.drawImage(image, -215, -400);
 //Player down and canvas width divided by x coordinate 2 and y coordinate 0.
-c.drawImage(playerImageDown,canvas.width / 2, canvas.height / 2);
+c.drawImage
+(playerImageDown,
+    0,
+    0,
+    playerImageDown.width / 4,
+    playerImageDown.height,
+ canvas.width / 2 - playerImageDown.width / 4 / 2,
+ canvas.height / 2 - playerImageDown.height / 2,
+ playerImageDown.width/ 4,
+ playerImageDown.height
+ )
+ //the equation above cuts the sprite sheet into one animation and it centers the character on the center coordinate?
 }
+
+function animate(){
+    window.requestAnimationFrame(animate)
+    c.drawImage(image, -215, -400);
+//Player down and canvas width divided by x coordinate 2 and y coordinate 0.
+c.drawImage
+(playerImageDown,
+    0,
+    0,
+    playerImageDown.width / 4,
+    playerImageDown.height,
+ canvas.width / 2 - playerImageDown.width / 4 / 2,
+ canvas.height / 2 - playerImageDown.height / 2,
+ playerImageDown.width/ 4,
+ playerImageDown.height
+ )
+console.log('animate')
+}
+
+animate()
+
+window.addEventListener('keydown', (e) =>{
+   
+    switch (e.key){
+        case 'w':
+            console.log('presend w key')
+            break
+
+            case 'a':
+                console.log('presend a key')
+                break
+
+                case 's':
+                    console.log('presend s key')
+                    break
+
+                    case 'd':
+                        console.log('presend d key')
+                        break
+    }
+})
  
 
