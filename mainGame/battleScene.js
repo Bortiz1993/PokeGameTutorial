3ff437a628141306b9818ce75f20d84f50c94dc3
Bortiz1993 <brijido.zuniga@bryanuniversity.edu>
2022-11-24 00:18:08 -0600

    button for attacks has a debounce function, monsters stay in place

--- mainGame/battleScene.js
+++ mainGame/battleScene.js
@@ -18,18 +18,17 @@ let queue
 // console.log(emby)
 // console.log(draggle)
 
-function debounce(cb, delay = 1000){
-  let timeout
+// function debounce(cb, delay = 1000){
+//   let timeout
 
-  //debounce waits one second before it the function fires
-
-  return(...args) => {
-      clearTimeout(timeout)
-      timeout = setTimeout(() => {
-          cb(...args)
-      }, delay)
-  }
-}
+//   //debounce waits one second before it the function fires
+//   return(...args) => {
+//       clearTimeout(timeout)
+//       timeout = setTimeout(() => {
+//           cb(...args)
+//       }, delay)
+//   }
+// }
 
 function initBattle()  {
 document.querySelector('#userInterface').style.display = 'block'
@@ -40,10 +39,7 @@ document.querySelector('#attacksBox').replaceChildren()
 
   draggle = new Monsters(monsters.Draggle)
   emby = new Monsters(monsters.Emby)
-  
-
   renderedSprites = [draggle, emby]
-
   queue = []
 
 emby.attacks.forEach((attack) =>{
@@ -53,104 +49,114 @@ emby.attacks.forEach((attack) =>{
 
 })
 
-const attackActions = (e) => {
+// const attackActions = (e) => {
 
-  console.log(e.currentTarget.innerHTML)
+
 
   //emby attack options, depends on what you click?
-  const selectedAttack = attacks[e.currentTarget.innerHTML]
-  emby.attack({
-  attack: selectedAttack,
-  recipient: draggle,
-  renderedSprites
-})
-
-if(draggle.health <= 0){
-  queue.push(() => {
-    console.log(draggle.position.y)
-    draggle.faint()
-    // draggle.draggleBattlePosition()
-    //I left off here, made this custom function.
-    // draggle.position.y - 20
-    
-  })
-  queue.push(() => {
-    //fades back to black
-    gsap.to('#overlappingDIV', {
-      opacity: 1,
-      onComplete: () => {
-        cancelAnimationFrame(battleAnimationId)
-        animate()
-        document.querySelector('#userInterface').style.display = 'none'
-        gsap.to('#overlappingDIV', {
-          opacity: 0
-        })
-        battle.initiated = false
-        audio.Map.play()
-       
+  document.querySelectorAll('button').forEach((button) => {
+    const debounce = (func, delay) =>{
+      let debounceTimer
+      return function() {
+        const context = this
+        const args = arguments
+        clearTimeout(debounceTimer)
+        debounceTimer
+        = setTimeout(() => func.apply(context, args), delay)
       }
-    })
-  })
-  // queue.push(() => {
-  //   draggle.position.y - 20
-  //   console.log(draggle.position.y, "draggle position faint")
+    }
+  const embyEvent =  (e)  => {
+      console.log(e, "is this one working 68?")
+
+   const selectedAttack = attacks[e.target.innerHTML]
+   //we changed the currentTarget to target in the event(e) object.
+   emby.attack({
+   attack: selectedAttack,
+   recipient: draggle,
+   renderedSprites
+ })
+
+ if(draggle.health <= 0) {
+   queue.push(() => {
+     console.log(draggle.position.y)
+     draggle.faint()
+     // draggle.draggleBattlePosition()
+     //I left off here, made this custom function.
+     // draggle.position.y - 20
+     })
+
+   queue.push(() => {
+     //fades back to black
+     gsap.to('#overlappingDIV', {
+       opacity: 1,
+       onComplete: () => {
+         cancelAnimationFrame(battleAnimationId)
+         animate()
+         document.querySelector('#userInterface').style.display = 'none'
+         gsap.to('#overlappingDIV', {
+           opacity: 0
+         })
+         battle.initiated = false
+         audio.Map.play()
+       }
+     })
+   })
   
-    ///TODO draggle shifts down and shifts up on final attack. Watch video
-  // })
-  
-}
+   ///SOMEWHAT DONE? draggle shifts down and shifts up on final attack. Watch video
+   // })  
+ }   
 
 //draggle or enemy attack options?
 const randomAttack =
 draggle.attacks[Math.floor(Math.random() * draggle.attacks.length)]
 
 queue.push(() => {
-  draggle.attack({
-      attack: randomAttack,
-      recipient: emby,
-      renderedSprites
-  })
+draggle.attack({
+   attack: randomAttack,
+   recipient: emby,
+   renderedSprites
+})
 
-  if(emby.health <= 0){
-    queue.push(() => {
-      emby.faint()
-    })
-    queue.push(() => {
-      //fades back to black
-      gsap.to('#overlappingDIV', {
-        opacity: 1,
-        onComplete: () => {
-          cancelAnimationFrame(battleAnimationId)
-          animate()
-          document.querySelector('#userInterface').style.display = 'none'
-          gsap.to('#overlappingDIV', {
-            opacity: 0
-          })
-          battle.initiated = false
-          audio.Map.play()
-        }
-      })
-    })
-  }
+if(emby.health <= 0){
+ queue.push(() => {
+   emby.faint()
+ })
+ queue.push(() => {
+   //fades back to black
+   gsap.to('#overlappingDIV', {
+     opacity: 1,
+     onComplete: () => {
+       cancelAnimationFrame(battleAnimationId)
+       animate()
+       document.querySelector('#userInterface').style.display = 'none'
+       gsap.to('#overlappingDIV', {
+         opacity: 0
+       })
+       battle.initiated = false
+       audio.Map.play()
+     }
+   })
+ })
+}
 })
 }
-
-const updateDebounceText = debounce((test) =>{
-  console.log(test)
-attackActions(test)
-})
+ 
+    button.addEventListener('click', debounce(function(e) {
+      console.log(e, "this event is at 154")
+      embyEvent(e)
+      // alert("Hello\nNo matter how many times you" +
+      //     "click the debounce button, I get " +
+      //     "executed once every 3 seconds!!")
+                      }, 800));
 
 //button and attack action functionality. Grabs all buttons, place objects inside of this button.{attack buttons}
-document.querySelectorAll('button').forEach((button) => {
-  button.addEventListener('click', (e) => updateDebounceText(e))
 
-button.addEventListener('mouseenter', (e) =>{
+button.addEventListener('mouseenter', (e) => {
     const selectedAttack = attacks[e.currentTarget.innerHTML]
     document.querySelector('#attackType').innerHTML = selectedAttack.type
     document.querySelector('#attackType').style.color = selectedAttack.color
 })
 })
-
 }
 // console.log(emby)
 // console.log(draggle)
@@ -174,7 +180,7 @@ animate();
 
 
 ///an event listener for the dialogue box.
-document.querySelector('#dialogueBox').addEventListener('click', (e) =>{
+document.querySelector('#dialogueBox').addEventListener('click', (e) => {
     if(queue.length > 0 ){
         queue[0]()
         queue.shift()
